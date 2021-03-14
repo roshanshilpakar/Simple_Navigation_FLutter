abdcab37a9959bd3ceb9da1b3d98c5a2e810588c
Roshan Shilpakar <roshan.shilpakar.2@gmail.com>
2021-03-14 22:09:10 +0530

    title

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Navigation Button"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Home Page"),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
